279db0e0729947a93be4d695a981fbb8604b9b2d
xtrm <oss@xtrm.me>
2024-01-08 03:49:55 +0100

    🐛 fix(cloudflare): del lock because ofc
    Signed-off-by: xtrm <oss@xtrm.me>

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno-v1.34.2 sh -s v1.34.2
+rm -f deno.lock
 NO_COLOR=1 DENO_VERSION=v1.34.2 \
     ./deno-v1.34.2/bin/deno task build --allow-all 
