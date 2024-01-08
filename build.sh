0a6e3e4d3e47211b6fcc52a2985cfa5b1e19531a
xtrm <oss@xtrm.me>
2024-01-08 03:47:11 +0100

    🐛 fix(cloudflare): build script?
    Signed-off-by: xtrm <oss@xtrm.me>

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno-v1.34.2 sh -s v1.34.2
+NO_COLOR=1 DENO_VERSION=v1.34.2 \
+    ./deno-v1.34.2/bin/deno task build --allow-all 
