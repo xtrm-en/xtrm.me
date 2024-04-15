903cd16bfe59958ab9ea1dd49b1a097845bdfdbb
xtrm <oss@xtrm.me>
2024-04-15 16:30:09 +0200

    ✨ feature(nix): added flake.nix + .envrc

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "dev-shell for x's website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            deno
+          ];
+        };
+      }
+    );
+}
+# vim: set ft=nix ts=2 sw=2 et:
+
